7c743f3e690b1629bc56bce08686b3208ec0627d
Yeyeando <sercacsal2@gmail.com>
2025-02-10 19:30:10 +0000

    informe funcional

--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -1,6 +1,6 @@
 import Footer from "../components/general/footer/Footer";
 import Header from "../components/general/header/Header";
-import ReporteUsuarios from "../components/reports/userTable";
+import ReporteUsuarios from "../components/reports/UserTable";
 import ContainsGraphics from "../components/reports/ContainsGraphics";
 function Report() {
   return (
